d620bbbc8a88d2f09b05f2ee619a9cfc9ab7a7de
Matt <mattmill1217@yahoo.com>
2023-01-07 22:41:32 -0600

    added flash messages in base.html, fixed login template with login form on login.html and same with signup.html. Updated auth.py to include the backend error messaging

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -11,6 +11,23 @@ def logout():
     return "<p>this is the logout page</p"
 
 
-@auth.route('/signup')
-def signup(): 
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error') 
+        elif len(firstname) < 2:
+            flash('First name must be greater than 2 characters', category='error') 
+        elif len(password1) != password2:
+            flash('Error, password mismatch', category='error')
+        elif len(password2) < 7: 
+            flash('Password must be more than 7 characters', category='error') 
+        else: 
+            flash('Account created!', category='success') 
+        
     return render_template("signup.html")
