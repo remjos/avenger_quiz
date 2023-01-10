1dd502d7afa4c36519747d4e181257f7161f1d56
Matt <mattmill1217@yahoo.com>
2023-01-09 20:08:52 -0600

    updated init.py and models.py to include database modeling and creation of those databases. Bug still persists for the create_database func

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
+from os import path
 
 db = SQLAlchemy
 DB_name = "database.db"
@@ -10,13 +11,22 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'uoihweiuhb ewfuewhfwefhewfuih'
     app.config['SQLALCHEMY_DATABSE_URI'] = f'sqlite:///{DB_name}'
-    db.init_app(app) 
-    
+    db.init_app(app)
+
     from .views import views 
     from .auth import auth 
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+
+    create_database(app)
     
     return app
+
+def create_database(app): 
+    if not path.exists('website'/ + DB_name): 
+        db.create_all(app=app)
+        print('Created the database!')
--- website/models.py
+++ website/models.py
@@ -1,9 +1,16 @@
 from . import db 
 from flask_login import UserMixin 
+from sqlalchemy.sql import func 
 
+class Note(db.model):
+    id = db.Column(db.Integer, primarykey=True)
+    data = db.Column(db.string(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now()) 
+    user_id = db.Column(db.integer, db.ForeignKey('user.id'))
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True) 
     password = db.Column(db.String(150))
     first_name = db.Column(db.string(150)) 
+    notes = db.relationship('Note') 
     
