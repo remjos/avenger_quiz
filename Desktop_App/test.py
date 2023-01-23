95ea03fd2ba68446b2393c6bc3b2aaa3c129f9db
Matt <mattmill1217@yahoo.com>
2023-01-23 13:25:42 -0600

    created button to move to new window

--- Desktop_App/test.py
+++ Desktop_App/test.py
@@ -8,23 +8,34 @@ app.title("The Avenger Quiz")
 customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("dark-blue")
 
-Frame1 = customtkinter.CTkFrame(master=app, width=500, height=500, corner_radius=10)
+def button_event():
+    window = customtkinter.CTkToplevel(text="New window")
+    window.geometry("600x500")
+
+Frame1 = customtkinter.CTkFrame(master=app, width=200, height=200, corner_radius=10)
 Frame1.pack(padx=50, pady=50)
-Label1 = customtkinter.CTkLabel(master=app, text="Welcome to the Avenger Quiz", fg_color=("white", "black"), corner_radius=10)
-Label1.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+Label1 = customtkinter.CTkLabel(master=app, anchor=customtkinter.CENTER, 
+    text="Welcome to the Avenger Quiz", 
+    fg_color=("white", "black", ), 
+    corner_radius=10)
+Label1.place(relx=0.5, rely=0.5, anchor='n')
+
+button2 = customtkinter.CTkButton(master=app, 
+    text="Click here to continue to THE AVENGER QUIZ", 
+    anchor=customtkinter.CENTER, 
+    command=button_event)
+button2.pack(padx=5, pady=5)
 
 Frame2 = customtkinter.CTkFrame(master=app)
 
-def button_event():
-    print("button pressed")
 
-button1 = customtkinter.CTkButton(master=Frame2)
 
+button1 = customtkinter.CTkButton(master=Frame2, command=button_event)
+button1.place(relx=0.5, rely=0.5)
 
 
 e1 = customtkinter.CTkEntry(master=app, placeholder_text="What is your eye color?")
-
-
 
 
 
